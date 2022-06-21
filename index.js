5672b727224fa21853fae6a1f3c013670c68c678
Himanshu Kabra <Himanshukabra2212@gmail.com>
2022-06-21 17:50:54 +0530

    commit 7

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+var a = 0;
 function slide()
 {
-    document.getElementById("notiposterid").style.width="32rem"
-    document.getElementById("notiposterid").style.transition="0.2s"
-
+    if(a%2 == 0)
+    {
+        document.getElementById("notiposterid").style.transition="0.2s"
+        document.getElementById("aboutevent").style.transition="0.2s"
+        document.getElementById("notiposterid").style.width="35rem"
+        document.getElementById("notiposterid").style.height="15rem"
+        document.getElementById("aboutevent").style.transform="rotate(180deg)"
+        a++
+    }
+    else
+    {
+        document.getElementById("notiposterid").style.transition="0.2s"
+        document.getElementById("aboutevent").style.transition="0.2s"
+        document.getElementById("notiposterid").style.width="70rem"
+        document.getElementById("notiposterid").style.height="30rem"
+        document.getElementById("aboutevent").style.transform="rotate(0deg)"
+        a--;
+    }
 }
