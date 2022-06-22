c513ddab13785fe1474cd4e6cb2ee4a5aee32084
Himanshu Kabra <Himanshukabra2212@gmail.com>
2022-06-22 10:22:53 +0530

    commit 9

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ function slide()
     if(a%2 == 0)
     {
         document.getElementById("notiposterid").style.transition="0.2s"
-        document.getElementById("aboutevent").style.transition="0.2s"
+        document.getElementById("abouteventicon").style.transition="0.2s"
         document.getElementById("notiposterid").style.width="35rem"
-        document.getElementById("notiposterid").style.height="15rem"
-        document.getElementById("aboutevent").style.transform="rotate(180deg)"
+        // document.getElementById("notiposterid").style.height="15rem"
+        document.getElementById("abouteventicon").style.transform="rotate(180deg)"
         a++
     }
     else
     {
         document.getElementById("notiposterid").style.transition="0.2s"
-        document.getElementById("aboutevent").style.transition="0.2s"
+        document.getElementById("abouteventicon").style.transition="0.2s"
         document.getElementById("notiposterid").style.width="70rem"
-        document.getElementById("notiposterid").style.height="30rem"
-        document.getElementById("aboutevent").style.transform="rotate(0deg)"
+        // document.getElementById("notiposterid").style.height="30rem"
+        document.getElementById("abouteventicon").style.transform="rotate(0deg)"
         a--;
     }
 }
