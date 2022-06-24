c671d21b912c61814e2ee24817660943f97a3d35
Himanshu Kabra <Himanshukabra2212@gmail.com>
2022-06-24 09:52:10 +0530

    commit 16

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var eventname = ["eventname1","eventname2","eventname3","eventname4","eventname5","eventname6"];
+var eventname = ["eventname1", "eventname2", "eventname3", "eventname4", "eventname5", "eventname6"];
 var i = 0;
 let eventlength = eventname.length;
 function myfunction() {
@@ -13,10 +13,9 @@ function myfunction() {
         document.getElementById("rightslide").style.opacity = "1";
     }
 }
-function evinfo()
-{
-    document.getElementById("notiposterid").style.backgroundImage = 'url("./content/event'+(i+1)+'.jpg")'
-    document.getElementById("eventname1").innerHTML = '<img src="content\\logo'+(i+1)+'.jpg" alt="nologo" id="eventlogo">';
+function evinfo() {
+    document.getElementById("notiposterid").style.backgroundImage = 'url("./content/event' + (i + 1) + '.jpg")'
+    document.getElementById("eventname1").innerHTML = '<img src="content\\logo' + (i + 1) + '.jpg" alt="nologo" id="eventlogo">';
 }
 function nextevent() {
     if (i < eventlength - 1) {
@@ -41,7 +40,7 @@ function slide() {
         document.getElementById("notiposterid").style.cssText = "transition: 0.2s; width: 35rem;  ";
         document.getElementById("abouteventicon").style.cssText = "transition: 0.2s; transform: rotate(180deg)"
         setTimeout(info, 300);
-        document.getElementById("notiposterid").style.backgroundImage = 'url("./content/event'+(i+1)+'.jpg")'
+        document.getElementById("notiposterid").style.backgroundImage = 'url("./content/event' + (i + 1) + '.jpg")'
         document.getElementById("eventinfo").style.cssText = "transition: 0.5s; transition-delay: 0s; width:35rem; height:34rem; display: inline-block; float: right; background-color: rgb(255,255,255,);"
         a++
     }
@@ -49,7 +48,7 @@ function slide() {
         document.getElementById("notiposterid").style.cssText = "transition: 0.2s; width: 70rem;"
         document.getElementById("abouteventicon").style.cssText = "transition: 0.2s; transform: rotate(0deg)"
         document.getElementById("eventinfo").style.cssText = "transition: 0.5s; transition-delay: 0s; width:0rem; height:34rem; display: inline-block; float: right; background-color: transparent;"
-        document.getElementById("notiposterid").style.backgroundImage = 'url("./content/event'+(i+1)+'.jpg")'
+        document.getElementById("notiposterid").style.backgroundImage = 'url("./content/event' + (i + 1) + '.jpg")'
         document.getElementById("eventinfo").innerHTML = ""
         a--;
     }
